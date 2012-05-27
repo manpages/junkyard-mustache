a5dbadc707757bf4babdbcd642571d8b3ea52789
Jonns Mostovoys <amarr.industrial@gmail.com>
2012-05-27 20:21:41 +0300

    things I left with after fucking the riak

--- erlang/apps/junkyard_mustache/src/jmus_helloworld.erl
+++ erlang/apps/junkyard_mustache/src/jmus_helloworld.erl
@@ -6,8 +6,7 @@ init({_Any, http}, Req, []) ->
 	{ok, Req, undefined}.
 
 handle(Req, State) ->
-	AbsolutePath = "/home/sweater/github/mine/junkyard-mustache/",
-	{ok, Req2} = cowboy_http_req:reply(200, [], mustache:render(jmus_view_index, AbsolutePath ++ "index.mustache"), Req),
+	{ok, Req2} = cowboy_http_req:reply(200, [], mustache:render(jmus_view_index, "html/index.mustache"), Req),
 	{ok, Req2, State}.
 
 terminate(_Req, _State) ->
